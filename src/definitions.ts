7d3f5c393a5a5476ff9d8e14de25006a2bc880a5
Premchandani <JPremchandani@dstworldwideservices.com>
2021-08-15 16:36:34 +0530

    Initial commit

---
+++ src/definitions.ts
@@ -0,0 +1,52 @@
+export enum BiometryType {
+  NONE,
+  TOUCH_ID,
+  FACE_ID,
+  FINGERPRINT,
+  FACE_AUTHENTICATION,
+  IRIS_AUTHENTICATION,
+}
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
+export interface AvailableResult {
+  isAvailable: boolean;
+  biometryType: BiometryType;
+}
+
+export interface BiometricOptions {
+  reason?: string;
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  negativeButtonText?: string;
+}
+
+export interface GetCredentialOptions {
+  server: string;
+}
+
+export interface SetCredentialOptions {
+  username: string;
+  password: string;
+  server: string;
+}
+
+export interface DeleteCredentialOptions {
+  server: string;
+}
+
+export interface NativeBiometricPlugin {
+  isAvailable(): Promise<AvailableResult>;
+
+  verifyIdentity(options?: BiometricOptions): Promise<any>;
+
+  getCredentials(options: GetCredentialOptions): Promise<Credentials>;
+
+  setCredentials(options: SetCredentialOptions): Promise<any>;
+
+  deleteCredentials(options: DeleteCredentialOptions): Promise<any>;
+}
